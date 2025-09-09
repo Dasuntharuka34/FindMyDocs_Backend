1bf19fc8738816e557fc575669000cd6b6a53f01
Dasuntharuka34 <152604324+Dasuntharuka34@users.noreply.github.com>
2025-09-10 01:49:42 +0530

    firebase implimentation

--- config/firebase.js
+++ config/firebase.js
@@ -1,20 +1,14 @@
 import admin from 'firebase-admin';
-import { readFileSync } from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Initialize Firebase Admin SDK
-const serviceAccount = JSON.parse(readFileSync(path.join(__dirname, '..', 'findmydocs-489b9-firebase-adminsdk-fbsvc-9b495add88.json'), 'utf8'));
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: 'findmydocs-489b9.appspot.com' // Replace with your Firebase Storage bucket name
+  storageBucket: 'findmydocs-489b9.appspot.com' // Your bucket name
 });
 
 const bucket = admin.storage().bucket();
 
 export { bucket };
 export default admin;
+

b62d273ba2e23c9dcd425315343e45878ec88c95
Dasuntharuka34 <152604324+Dasuntharuka34@users.noreply.github.com>
2025-11-19 00:13:30 +0530

    `Removed unused email sending functionality and removed verifyEmail route`

--- routes/registrationRoutes.js
+++ routes/registrationRoutes.js
@@ -2,8 +2,7 @@ import express from 'express';
 import {
     createRegistration,
     deleteRegistration,
-    getPendingRegistrations,
-    verifyEmail,
+    getPendingRegistrations
 } from '../controllers/registrationController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 const router = express.Router();
@@ -11,6 +10,5 @@ const router = express.Router();
 router.route('/pending').get(protect, admin, getPendingRegistrations);
 router.route('/').post(createRegistration);
 router.route('/:id').delete(protect, admin, deleteRegistration);
-router.route('/verify/:token').get(verifyEmail);
 
 export default router;
